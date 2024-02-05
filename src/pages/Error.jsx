b87e141dbb96ec117c7e1d3cdb6ebac041f5158c
kosticivan5 <kosticivan5@gmail.com>
2024-02-05 10:45:28 +0300

    added 404 page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>404</div>;
+  return (
+    <main className="error-page">
+      <h1>404</h1>
+      <p>Упсс...такая страница не существует</p>
+      <Link to={"/"}>На главную</Link>
+    </main>
+  );
 };
 export default Error;
